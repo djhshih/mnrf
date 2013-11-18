0ae2d6fd297d8d960eb30709afb366dd0da4f656
David JH Shih <djh.shih@gmail.com>
2013-11-18 16:55:46 -0500

    Added support for multiple tracks.

--- mnrf.cpp
+++ mnrf.cpp
@@ -16,68 +16,115 @@
 using namespace std;
 
 
+const size_t buffer_size = 1024;
+
+
+bool get_fasta_header(const char* line, char* header) {
+
+	// header line begins with '>'
+	// otherwise, current line is not a headerline
+	if (line[0] != '>') {
+		return false;
+	}
+
+	// find first space character
+	size_t count = 0;
+	for (size_t i = 1; i < buffer_size; ++i) {
+		if (line[i] == ' ') break;
+		++count;
+	}
+	// copy to header
+	memcpy(header, &(line[1]), count);
+	header[count] = '\0';
+
+	return true;
+}
+
+
 void mnrf(unsigned period, istream& in, ofstream& out) {
 
 	const char delim = '\t';
-	const size_t buffer_size = 1024;
+	const unsigned track_name_size = 32;
+	const char ignore_nuc = 'N';
+
+	char track_name[track_name_size];
 	char buffer[buffer_size];
-	char chromosome[8];
 
-	// assume only one track
+	size_t count = 1;
+	size_t start, pos;
+	char prev_nuc;
 
-	/// get track name
+	track_name[0] = '\0';
 
-	in.getline(buffer, buffer_size);
-	if (buffer[0] != '>') {
-		throw runtime_error("Error: FASTA must have a track name");
-	}
-
-	// replace first space with null
-	for (size_t i = 1; i < buffer_size; ++i) {
-		if (buffer[i] == ' ') {
-			buffer[i] = '\0';
-			break;
-		}
-	}
-	// copy to chromosome
-	strcpy(chromosome, &(buffer[1]));
-
-
-	size_t start = 0, pos = 0, count = 0;
-	char prev_nuc = '\0';
-
-	
 	while (!in.eof()) {
 
-		// get next line
 		in.getline(buffer, buffer_size);
-		if (buffer[0] == '\0') break;
 
-		for (size_t i = 0; i < buffer_size; ++i) {
+		// ignore empty lines
+		if (buffer[0] == '\0') continue;
 
-			if (buffer[i] == '\0') break;
+		if (get_fasta_header(buffer, track_name)) {
 
-			if (buffer[i] == prev_nuc) {
-				++count;
-			} else {
+			/// succeeded in getting header: new track
 
-				if (count >= period) {
-					// record current mononucleotide stretch
-					out
-						<< chromosome << delim
-						<< start << delim
-						<< (start + count) << delim
-						<< prev_nuc << delim
-						<< count << endl;
-				}
+			// clean up last stretch
+			if (count >= period) {
+				// record current mononucleotide stretch
+				out
+					<< track_name << delim
+					<< start << delim
+					<< (start + count) << delim
+					<< prev_nuc << delim
+					<< count << endl;
+			}
+			
+			// initialize for new track
+			start = pos = 0;
+			prev_nuc = '\0';
+			count = 1;
 
-				// prepare for next mononucleotide stretch
-				prev_nuc = buffer[i];
-				start = pos;
-				count = 1;
+		} else {
+
+			/// continue with current track
+
+			if (track_name[0] == '\0') {
+				throw runtime_error("Error: FASTA must have at least one track name");
 			}
 
-			++pos;
+			for (size_t i = 0; i < buffer_size; ++i) {
+
+				char nucleotide = buffer[i];
+
+				if (nucleotide == '\0') break;
+
+				if (nucleotide == ignore_nuc) continue;
+
+				if (nucleotide == prev_nuc) {
+
+					++count;
+
+				} else {
+
+					// clean up last stretch
+					if (count >= period) {
+						// record current mononucleotide stretch
+						out
+							<< track_name << delim
+							<< start << delim
+							<< (start + count) << delim
+							<< prev_nuc << delim
+							<< count << endl;
+					}
+
+					// prepare for next mononucleotide stretch
+					prev_nuc = buffer[i];
+					start = pos;
+					count = 1;
+				}
+
+				++pos;
+
+			}
 
 		}
 
@@ -86,7 +133,12 @@ void mnrf(unsigned period, istream& in, ofstream& out) {
 	// clean up last stretch
 	if (count >= period) {
 		// record current mononucleotide stretch
-		out << chromosome << delim << start << delim << (start + count) << delim << prev_nuc << endl;
+		out
+			<< track_name << delim
+			<< start << delim
+			<< (start + count) << delim
+			<< prev_nuc << delim
+			<< count << endl;
 	}
 
 }
