7db0baa364221bfa9f23f4cb965474b05d091dd6
David JH Shih <djh.shih@gmail.com>
2013-11-18 17:09:39 -0500

    Added license.

--- mnrf.cpp
+++ mnrf.cpp
@@ -1,11 +1,21 @@
-/**
- * Mononucleotide repeat finder
- *
- * Author:   David JH Shih <djh.shih@gmail.com>
- * Date:     2013-11-18
- * License:  GPLv3
- *
- */
+/*
+    Mononucleotide repeat finder.
+    Copyright (C) 2013  David JH Shih <djh.shih@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include <cstdlib>
 #include <iostream>
